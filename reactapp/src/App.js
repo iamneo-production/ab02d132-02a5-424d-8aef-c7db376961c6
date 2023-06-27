7bc5d9cb605955d64fce164efb067088bceb5965
Vivek <74505677+Vivek-3009@users.noreply.github.com>
2023-06-28 03:19:13 +0530

    Update App.js

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,11 @@
 
 import './App.css';
-<<<<<<< HEAD
 import UserForm from './components/UserForm';
 function App() {
   return (
     <div className="App">
      <UserForm/>
     </div>
-=======
-import { Login } from './Components/UI/Login';
-
-function App() {
-  return (
-    <>
-      <Login></Login>
-    </>
->>>>>>> Project-Workspace-Vivek-3009
   );
 }
 
