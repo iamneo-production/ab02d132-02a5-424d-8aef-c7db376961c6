6fc76f5cc9c5e073f2bf799eb146347f61c228a3
Vivek-3009 <vivek.bisht555@gmail.com>
2023-06-27 21:15:57 +0000

    m

---
+++ reactapp/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+<<<<<<< HEAD
+import UserForm from './components/UserForm';
+function App() {
+  return (
+    <div className="App">
+     <UserForm/>
+    </div>
+=======
+import { Login } from './Components/UI/Login';
+
+function App() {
+  return (
+    <>
+      <Login></Login>
+    </>
+>>>>>>> Project-Workspace-Vivek-3009
+  );
+}
+
+export default App;
