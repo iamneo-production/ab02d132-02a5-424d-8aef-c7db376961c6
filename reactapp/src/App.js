aaa3bab672c06a2b9258c451e2cda11903df0a52
sweta23092001 <skumari50350@gmail.com>
2023-06-28 09:29:00 +0000

    Navbar1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,87 @@
 
 import './App.css';
+// import AdminNavbar from './components/AdminNavbar';
 import UserForm from './components/UserForm';
+// import {
+//   BrowserRouter as Router,
+//   // Switch,
+//   Route,
+//   // Link,
+//   Routes
+// } from "react-router-dom";
+
+// import HomePageNavbar from './components/HomePageNavbar';
+// import TeamLeaderNavbar from './components/TeamLeaderNavbar';
+// import TeamMemberNavbar from './components/TeamMemberNavbar';
+
 function App() {
   return (
-    <div className="App">
-     <UserForm/>
-    </div>
+    <>
+      <div className="App">
+        <UserForm/>
+        {/* <AdminNavbar/>
+        <HomePageNavbar/>
+        <TeamLeaderNavbar/>
+        <TeamMemberNavbar/> */}
+
+        {/* <Router>
+          <HomePageNavbar/>
+          <div className="container my-3" >
+            <Routes>
+              <Route exact path="/userform" element={<UserForm />}>
+              </Route>
+              <Route exact path="/userlist" element={<UserList/>}>
+              </Route>
+              <Route exact path="/logout" element={<Logout/>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router> */}
+
+        {/* <Router>
+          <AdminNavbar/>
+          <div className="container my-3" >
+            <Routes>
+              <Route exact path="/userform" element={<UserForm />}>
+              </Route>
+              <Route exact path="/userlist" element={<UserList/>}>
+              </Route>
+              <Route exact path="/logout" element={<Logout/>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router> */}
+
+        {/* <Router>
+          <TeamLeaderNavbar/>
+          <div className="container my-3" >
+            <Routes>
+              <Route exact path="/userform" element={<UserForm />}>
+              </Route>
+              <Route exact path="/userlist" element={<UserList/>}>
+              </Route>
+              <Route exact path="/logout" element={<Logout/>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router> */}
+
+        {/* <Router>
+          <TeamMemberNavbar/>
+          <div className="container my-3" >
+            <Routes>
+              <Route exact path="/userform" element={<UserForm />}>
+              </Route>
+              <Route exact path="/userlist" element={<UserList/>}>
+              </Route>
+              <Route exact path="/logout" element={<Logout/>}>
+              </Route>
+            </Routes>
+          </div>
+        </Router> */}
+
+      </div>
+    </>
   );
 }
 
---
+++ reactapp/src/components/HomePageNavbar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default function HomePageNavbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg  navbar-light " style={{ backgroundColor: "#e3f2fd" }}>
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">
+                        <img src="./images/logo.png" width="" height="30" />
+                    </a>
+                    <div className="d-flex" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Login</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Register</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact us</a>
+                            </li>
+
+                        </ul>
+
+
+
+                    </div>
+                </div>
+            </nav>
+    </div>
+  )
+}
