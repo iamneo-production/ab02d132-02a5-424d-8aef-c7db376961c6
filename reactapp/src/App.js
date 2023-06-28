c63356a3b7dfe03576450904c482e560e2e22ecc
jsree01 <sec19ic015@sairamtap.edu.in>
2023-06-28 12:22:37 +0000

    1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-// import UserForm from './components/UserForm';
+
 import UserList from './components/UserList';
 
 export default function App() {
   return (
     <div>
-      {/* <UserForm/> */}
+      
       <UserList/>
     </div>
   );
