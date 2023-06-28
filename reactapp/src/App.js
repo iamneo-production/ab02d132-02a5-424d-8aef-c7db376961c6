146d2016f49362d3b332bb511b9f52aaf01d289f
jsree01 <sec19ic015@sairamtap.edu.in>
2023-06-28 10:56:21 +0000

    First commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,12 @@
-
-import './App.css';
+import React from 'react';
 // import UserForm from './components/UserForm';
-import UserList_member from './components/UserList_member';
-function App() {
+import UserList from './components/UserList';
 
+export default function App() {
   return (
-    <div className="App">
-     {/* <UserForm/> */}
-     <UserList_member/>
+    <div>
+      {/* <UserForm/> */}
+      <UserList/>
     </div>
   );
 }
-
-export default App;
--- reactapp/src/components/UserList_member.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UserList_member() {
-  return (
-    <div>UserList_member</div>
-  )
-}
-
-export default UserList_member
