c5cf8c0f2f3db71c9deac9224c17d6c9dbd1877a
Rishu-Gangishetti <gangishettirishu@gmail.com>
2023-06-29 09:55:45 +0000

    Final Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import AssignTask from './components/TaskDetails';
+import Home from './components/Home';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+     <AssignTask/>
+    </div>
+  );
+}
+
+export default App;
