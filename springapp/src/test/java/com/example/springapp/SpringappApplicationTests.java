2dd681af3c1e26c1930346c46448bee2188aa648
Vivek-3009 <vivek.bisht555@gmail.com>
2023-06-29 07:03:29 +0000

    springapp

--- springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
+++ springapp/src/test/java/com/example/springapp/SpringappApplicationTests.java
@@ -114,3 +114,16 @@ class SpringappApplicationTests {
       assertTrue(file.exists() && file.isFile());;
       }
 }
+package com.example.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
