31e8fbb5d2ff48650085761a1197d1935781c5d5
KeerthanaParanthagan-02 <keerthanaparanthagan@gmail.com>
2023-06-29 17:15:11 +0000

    second commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-import TaskDetails from './components/TaskDetails';
-import Home from './components/Home';
+// import TaskDetails from './components/TaskDetails';
+// import Home from './components/Home';
+import UserForm from './components/UserForm';
 function App() {
   return (
     <div className="App">
-      <Home/>
-     <TaskDetails/>
+      {/* <Home/> */}
+     {/* <TaskDetails/> */}
+     <UserForm/>
     </div>
   );
 }
