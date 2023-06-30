e78b63f00d93c0efd19ea1eb66a29aa8f78fcb67
oindrila-chatterjee <chatterjee.oindrila2k@gmail.com>
2023-06-30 12:25:57 +0000

    comments added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import UserList1 from './components/UserList1';
 // import UserForm from './components/UserForm';
 
-
+// I have added the files and resolved merge conflicts
 function App() {
   return (
     <div className="App">
