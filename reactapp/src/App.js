915253aba0c808d7d77906239739338df07706dc
oindrila-chatterjee <chatterjee.oindrila2k@gmail.com>
2023-06-30 12:29:50 +0000

    commit and sync

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import UserList1 from './components/UserList1';
 // import UserForm from './components/UserForm';
 
-// I have added the files and resolved merge conflicts
+
 function App() {
   return (
     <div className="App">
@@ -14,3 +14,51 @@ function App() {
 }
 
 export default App;
+import React from 'react';
+
+<<<<<<< HEAD
+import './App.css';
+import UserList1 from './components/UserList1';
+// import UserForm from './components/UserForm';
+<<<<<<< HEAD
+<<<<<<< HEAD
+import TaskList from './components/TaskList';
+=======
+import { Userlist_Leader } from './components/Userlist_Leader';
+>>>>>>> a0e1092 (myfirstcommit)
+=======
+
+
+>>>>>>> eb20de7 (UserList1)
+function App() {
+  return (
+    <div className="App">
+     {/* <UserForm/> */}
+<<<<<<< HEAD
+<<<<<<< HEAD
+     <TaskList/>
+=======
+
+
+
+import AssignTask from './components/TaskDetails';
+
+import TaskDetails from './components/TaskDetails';
+
+import Home from './components/Home';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+
+>>>>>>> 42b8b7d97041c8389fc69ee98cc59595acd06a9b
+=======
+     <Userist_leader/>
+>>>>>>> a0e1092 (myfirstcommit)
+=======
+     <UserList1/>
+>>>>>>> eb20de7 (UserList1)
+    </div>
+
+  );
+}
