c1729bcce51fd307ffde0a003079c854d6860b17
oindrila-chatterjee <chatterjee.oindrila2k@gmail.com>
2023-07-04 17:26:44 +0000

    changes in UserList1

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import UserList from './components/UserList';
 import UserForm from './components/UserForm';
+import UserList1 from './components/UserList1';
 function App() {
   return (
     <div>
       <UserForm/>
       <UserList/>
+      <UserList1/>
     </div>
 
   );
 }
+export default App;
