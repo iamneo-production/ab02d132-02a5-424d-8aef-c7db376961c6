258a26360d0c6a351e3539e856d227e76d66c3cb
jsree01 <sec19ic015@sairamtap.edu.in>
2023-07-07 18:21:56 +0000

    update

--- springapp/src/main/java/com/example/springapp/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springapp.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-  public ResourceNotFoundException(String msg) {
-    super(msg);
-  }
-}
