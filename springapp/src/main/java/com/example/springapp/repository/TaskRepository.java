aef514a85288564cbabab7f50fe439798a21d0f0
jsree01 <sec19ic015@sairamtap.edu.in>
2023-07-07 18:43:37 +0000

    ALL TEST CASES PASSED!

---
+++ springapp/src/main/java/com/example/springapp/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.example.springapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springapp.model.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    
+}
