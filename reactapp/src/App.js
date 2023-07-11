55fc08e846f78c1deb864b79930eef8a20b57bf6
KeerthanaParanthagan-02 <keerthanaparanthagan@gmail.com>
2023-07-11 15:27:13 +0000

    changes in userform

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <UserForm/>
-      <UserList/>
+      {/* <UserList/> */}
     </div>
   );
 }
--- reactapp/src/components/UserForm.js
+++ reactapp/src/components/UserForm.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React from 'react'
 
 import {
   MDBBtn,
@@ -12,23 +12,7 @@ import {
 } from 'mdb-react-ui-kit';
 
 function UserForm() {
-    const [value, setValue] = useState('')
-    const [value1, setValue1] = useState('')
-    const [value2, setValue2] = useState('')
-    const [value3, setValue3] = useState('')
-    const [value4, setValue4] = useState('')
-    const [displaycolor,setcolor] = useState('')
-    const[task,setTask]=useState([])
-   const handleSubmit=(event)=>{
-        if(value!==''){
-            setTask([...task,value2,value3,value4,value,value1,displaycolor])
-            setValue('')
-            setValue1('')
-            setcolor('')
-
-        }
-       event.preventDefault(); 
-    };
+   
     
 
   return (
@@ -41,35 +25,26 @@ function UserForm() {
 
     <MDBCard className='my-5 cascading-right' style={{background: 'hsla(0, 0%, 100%, 0.55)',  backdropFilter: 'blur(30px)'}}>
     <MDBCardBody className='p-5 shadow-5 text-center'>
-           <h2 className="fw-bold  mb-5"> User Account</h2>
+           <h2 className="fw-bold  mb-5">Create User Account</h2>
         
 
            <MDBInput wrapperClass='mb-4' label='UserName' id='form3' type='text'
-           value={value2}
-           onChange={(e)=>setValue2(e.target.value)} />
+           />
            
            <MDBInput wrapperClass='mb-4' label='Email' id='form3' type='Email'
-           value={value4}
-           onChange={(e)=>setValue4(e.target.value)} />
+           />
            <MDBInput wrapperClass='mb-4' label='Password' id='form3' type='Password'
-           value={value3}
-           onChange={(e)=>setValue3(e.target.value)} />
+            />
            
           
-  <MDBInput wrapperClass='mb-4' label='Task' id='form3' type='text' 
-        value={value}
-        onChange={(e)=>setValue(e.target.value)}
-        />
-        <MDBInput wrapperClass='mb-4' label='DueDate' id='form4' type='date'
-        value1={value1}
-        onChange={(e)=>setValue1(e.target.value)}
-        />
+ 
+        
       
      
      <>
-      <MDBRadio btn btnColor='secondary' id='btn-radio1' name='options' wrapperTag='span' label="Leader" onChange={(e)=>setcolor(e.target.value)} value="Leader"/>
+      <MDBRadio btn btnColor='secondary' id='btn-radio1' name='options' wrapperTag='span' label="Leader"  />
 
-      <MDBRadio btn btnColor='secondary' id='btn-radio2' name='options' wrapperClass='mx-2' wrapperTag='span' label='Member' onChange={(e)=>setcolor(e.target.value)}  value="Member"/>
+      <MDBRadio btn btnColor='secondary' id='btn-radio2' name='options' wrapperClass='mx-2' wrapperTag='span' label='Member' />
 
      
       </>
@@ -77,14 +52,12 @@ function UserForm() {
     <br/>
    
 
-        {/* <div className='d-flex justify-content-center mb-4'>
-          <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Confirm changes' />
-        </div> */}
+        
 <>
-    <MDBBtn className='mx-2 w-25' size='md 'wrapperTag='span'margin onClick={handleSubmit}>Create</MDBBtn>
+    <MDBBtn className='mx-2 w-25' size='md 'wrapperTag='span'margin>Create</MDBBtn>
       
-      <MDBBtn MDBBtn className='mx-2 w-25' size='md 'wrapperTag='span' onClick={handleSubmit}>
-       Update
+      <MDBBtn MDBBtn className='mx-2 w-25' size='md 'wrapperTag='span' >
+       Cancel
       </MDBBtn>
 </>
         
@@ -98,11 +71,7 @@ function UserForm() {
       </MDBCol>
       </MDBRow>
       </MDBContainer>
-      <div>
-         <p>
-        {task.map(task=> - <p  value={task}>{task}</p>)}
-      </p>
-      </div>
+      
      
       </form>
      
