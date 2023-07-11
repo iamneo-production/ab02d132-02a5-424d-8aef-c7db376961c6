cfee2a65f60c660ba9e7ec1c44ca098c68fe3c3b
sweta23092001 <skumari50350@gmail.com>
2023-07-11 12:37:18 +0000

    empty commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,12 +5,7 @@ import UserForm from './components/UserForm';
 import { Login } from './components/Login';
 function App() {
   return (
-    <div>
       <UserForm/>
       {/* <UserList/> */}
     </div>
 
-  );
-}
-export default App;
-  
