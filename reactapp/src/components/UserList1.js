637511cb7645de9f5a4298501919ed6087d94856
jsree01 <sec19ic015@sairamtap.edu.in>
2023-07-16 08:23:32 +0000

    Added some rows in UserList1.js

--- reactapp/src/components/UserList1.js
+++ reactapp/src/components/UserList1.js
@@ -3,6 +3,7 @@ import {  MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit'
 
 function UserList1() {
   return (
+    
     <MDBTable align='middle'>
       <MDBTableHead>
         <tr>
@@ -13,6 +14,29 @@ function UserList1() {
         </tr>
       </MDBTableHead >
       <MDBTableBody>
+        <tr>
+          <td>
+            {/* <div className='d-flex align-items-center'> */}
+              
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>1</p>
+                
+              </div>
+            
+          </td>
+          <td>
+            <p className='fw-normal mb-1'>abc</p>
+    
+          </td>
+          <td>
+          <p className='text-muted mb-0'>task 1</p>
+          </td>
+          
+          <td>
+          <MDBBtn rounded size='sm' className="btn btn-primary">approved</MDBBtn>
+              <MDBBtn rounded size='sm' className="btn btn-danger ">not approved</MDBBtn>
+          </td>
+        </tr>
         <tr>
           <td>
             {/* <div className='d-flex align-items-center'> */}
